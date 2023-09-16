9620584967bb536d4d50230dd9e4578ef612354b
Daniel Barros <daniel.barros@cqumail.com>
2023-09-16 12:01:24 +0800

    Creating a Storage-Aware Cart Class

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -3,11 +3,13 @@
     public class Cart
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
-        public void AddItem(Product product, int quantity)
+
+        public virtual void AddItem(Product product, int quantity)
         {
             CartLine? line = Lines
-            .Where(p => p.Product.ProductID == product.ProductID)
-            .FirstOrDefault();
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
             if (line == null)
             {
                 Lines.Add(new CartLine
@@ -21,11 +23,14 @@
                 line.Quantity += quantity;
             }
         }
-        public void RemoveLine(Product product) =>
-        Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
-        public decimal ComputeTotalValue() =>
-        Lines.Sum(e => e.Product.Price * e.Quantity);
-        public void Clear() => Lines.Clear();
+
+        public virtual void RemoveLine(Product product) =>
+            Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+        public virtual decimal ComputeTotalValue() =>
+            Lines.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual void Clear() => Lines.Clear();
     }
 
     public class CartLine
